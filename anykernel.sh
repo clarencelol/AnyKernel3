3bd0dd8f15e413fea686336f1cf8c5c0bebf196a
clarencelol <clarencekuiek@proton.me>
2023-12-14 11:03:15 +0800

    anykernel3: its kinesis now

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Melon Kernel by Clarence
+kernel.string=kinesis Kernel by clarencelol
 do.devicecheck=1
 do.modules=0
 do.systemless=1
